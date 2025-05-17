1c68e0174a02bc53e1130b62ffe23fbb8bf09b1c
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-17 21:20:11 +0530

    Adding sample data to checking in frontend. in this sample data added in habits.component.ts

--- src/app/components/habits/habits.component.ts
+++ src/app/components/habits/habits.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './habits.component.css'
 })
 export class HabitsComponent {
-
+  habits = [
+    {id: 1, name: 'Morning Exercise',description: "30 minutes run",completed: true},
+    {id: 2, name: 'Books Reading',description: "Read 10 Pages",completed: true},
+    {id: 3, name: 'Drink Water',description: "8 Glasses a day",completed: false},
+  ]
 }
