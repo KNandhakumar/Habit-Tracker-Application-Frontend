97bcf23387e6ae3e566d45c4188a76326c238c60
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-17 21:01:00 +0530

    Adding routes for habit component is base path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HabitsComponent } from './components/habits/habits.component';
 
 export const routes: Routes = [
-
+    {path: '',component: HabitsComponent}
 ];
