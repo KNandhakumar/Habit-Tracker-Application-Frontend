3cfce1d82ba53d800e1ea3277c0bcf39f266312d
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-19 18:01:52 +0530

    Get all habits using this service

--- src/app/services/habit.service.ts
+++ src/app/services/habit.service.ts
@@ -1,9 +1,22 @@
+import { Habit } from './../model/habit.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitService {
 
-  constructor() { }
+  // this is use for integrate between frontend to backend
+  constructor(private http:HttpClient) { }
+
+  // backend api url
+  private apiUrl = 'http://localhost:8080/api/habits';
+
+  // getting all habits from backend
+  getHabits():Observable<Habit[]>{
+    return this.http.get<Habit[]>(this.apiUrl);
+  };
+
 }
