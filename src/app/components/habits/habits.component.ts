41767c5ae1eca2259632a5eac22d095cd51aa9e4
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-20 13:32:08 +0530

    added create habit new feature

--- src/app/components/habits/habits.component.ts
+++ src/app/components/habits/habits.component.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Habit } from '../../model/habit.model';
@@ -5,7 +6,7 @@ import { HabitService } from '../../services/habit.service';
 
 @Component({
   selector: 'app-habits',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './habits.component.html',
   styleUrl: './habits.component.css'
 })
@@ -18,6 +19,21 @@ export class HabitsComponent implements OnInit{
   ngOnInit(): void {
     this.habitService.getHabits().subscribe((data) => {
       this.habits = data;
+    });
+  };
+
+  // creating habits
+  newHabit:Habit = {
+    name: '',
+    description: '',
+    completed: false
+  };
+
+  addHabit(){
+    if (!this.newHabit.name || !this.newHabit.description) return;
+    this.habitService.createHabit(this.newHabit).subscribe((habit) => {
+      this.habits.push(habit);
+      this.newHabit = {name: '',description: '', completed: false};
     });
   };
 
